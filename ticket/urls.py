d1e06336229f6f30da408f54634b39b3414bfd7c
Baptiste <baptiste.ossipow@gmail.com>
2013-09-12 09:40:08 +0200

    end of ch 2 - tracker more or less

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import patterns, url
-from views import home, ticket_listing
+from views import home, ticket_listing, ticket_detail, ticket_list
 
 urlpatterns = patterns('',
                        url(r'^home/$', home, name = "home"),
                        url(r'^home/(\w+)$', home, name = "home"),
-                       url(r'^$', ticket_listing, name="listing")
+                       url(r'^$', ticket_listing, name="listing"),
+                       url(r'^detail/(\d+)', ticket_detail, name='detail'),
+                       url(r'^list/$', ticket_list, name='list')
 
 )
--- ticket/views.py
+++ ticket/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
+from datetime import datetime
 
 from .models import Task
 
@@ -9,6 +10,39 @@ def home(request, name=None):
     else:
         return HttpResponse("I'm a rockstar")
 
+
 def ticket_listing(request):
     objets = Task.objects.all().order_by('-due_date')
     return render_to_response('ticket/list.html', { 'objects': objets})
+
+
+def ticket_detail(request, id):
+    objet = Task.objects.get(pk=id)
+    return render_to_response('ticket/detail.html', {'objet': objet})
+
+
+def ticket_list(request):
+    format = "%Y-%m-%d"
+    objets = Task.objects.all()
+    if request.method == 'GET':
+        if 'closed' in request.GET:
+            if request.GET['closed'] == 'true':
+                objets = objets.filter(closed=True)
+            else:
+                objets = objets.filter(closed=False)
+                if 'start' in request.GET and request.GET['start'] != '':
+                    objets = objets.filter(
+                        due_date__gte=datetime.strptime(
+                            request.GET['start'], format)
+                        )
+                if 'end' in request.GET and request.GET['end'] != '':
+                    objets = objets.filter(
+                        due_date__lte=datetime.strptime(
+                            request.GET['end'], format)
+                        )
+    return render(request, 'ticket/list.html',
+                  { 'objets': objets },
+                  )
+                    
+
+
